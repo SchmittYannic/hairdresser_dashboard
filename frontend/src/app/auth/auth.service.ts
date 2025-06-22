c95e545ae735bd9627310511bed55cf3dc4c6132
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-06-22 21:21:49 +0200

    Fixes wrong apiUrl in auth.service

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -5,11 +5,11 @@ import { environment } from 'environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = `${environment}/auth`; // Backend-URL
+  private apiUrl = `${environment.apiUrl}/auth`; // Backend-URL
 
   constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { username, password });
+  signin(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signin`, { username, password });
   }
 }
