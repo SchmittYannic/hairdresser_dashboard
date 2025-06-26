cd773be1b0bec5dc9afebee7a26f2fd2af6fb612
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-06-26 14:37:49 +0200

    Adds sidebar-content to sidebar

--- frontend/src/app/layout/dashboard-layout/sidebar/sidebar.module.ts
+++ frontend/src/app/layout/dashboard-layout/sidebar/sidebar.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SidebarComponent } from './sidebar.component';
 import { SidebarLogoComponent } from './sidebar-logo/sidebar-logo.component';
+import { SidebarContentComponent } from './sidebar-content/sidebar-content.component';
 
 @NgModule({
   declarations: [
     SidebarComponent,
     SidebarLogoComponent,
+    SidebarContentComponent,
   ],
   imports: [CommonModule],
   exports: [SidebarComponent]
