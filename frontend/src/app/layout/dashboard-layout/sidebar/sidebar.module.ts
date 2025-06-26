ea8fb24dd58aaae004552b08068006de21bb9dcd
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-06-26 17:01:11 +0200

    adds imports to sidebar module

--- frontend/src/app/layout/dashboard-layout/sidebar/sidebar.module.ts
+++ frontend/src/app/layout/dashboard-layout/sidebar/sidebar.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SidebarComponent } from './sidebar.component';
 import { SidebarLogoComponent } from './sidebar-logo/sidebar-logo.component';
 import { SidebarContentComponent } from './sidebar-content/sidebar-content.component';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { SidebarGroupComponent } from './sidebar-group/sidebar-group.component';
+import { SidebarItemComponent } from './sidebar-item/sidebar-item.component';
 
 @NgModule({
   declarations: [
     SidebarComponent,
     SidebarLogoComponent,
     SidebarContentComponent,
+    SidebarGroupComponent,
+    SidebarItemComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NgScrollbarModule,
+    RouterModule,
+  ],
   exports: [SidebarComponent]
 })
 export class SidebarModule { }
