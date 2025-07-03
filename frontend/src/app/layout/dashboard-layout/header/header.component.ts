f7f0d140711370ce382204e9750f6fb248538e6c
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-03 18:12:36 +0200

    Implements header component

--- frontend/src/app/layout/dashboard-layout/header/header.component.ts
+++ frontend/src/app/layout/dashboard-layout/header/header.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardLayoutService } from '../dashboard-layout.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  imports: [
+    CommonModule,
+  ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isNotificationsOpen = false;
+  isUserDropdownOpen = false;
 
+  constructor(public layout: DashboardLayoutService) { }
+
+  handleNotificationToggleClick(event: MouseEvent) {
+    event.preventDefault();
+    this.layout.toggleNotificationsOpen();
+    this.layout.setUserDropdownOpen(false);
+  }
+
+  handleUserDropdownToggleClick(event: MouseEvent) {
+    event.preventDefault();
+    this.layout.toggleUserDropdownOpen();
+    this.layout.setNotificationsOpen(false);
+  }
+
+  ngOnInit(): void {
+    this.layout.isNotificationsOpen$.subscribe(val => this.isNotificationsOpen = val);
+    this.layout.isUserDropdownOpen$.subscribe(val => this.isUserDropdownOpen = val)
+  }
 }
