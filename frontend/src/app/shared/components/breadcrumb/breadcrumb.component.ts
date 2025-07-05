bbfe33835b2bcb023de71a16141c7efa606146fd
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-05 17:17:11 +0200

    Adds breadcrumb component

---
+++ frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+interface Breadcrumb {
+  label: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-breadcrumb',
+  standalone: true,
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.scss',
+  imports: [
+    CommonModule,
+    RouterModule,
+  ]
+})
+export class BreadcrumbComponent implements OnInit {
+  breadcrumbs: Breadcrumb[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.breadcrumbs = this.buildBreadcrumbs(this.route.root);
+      });
+
+    this.breadcrumbs = this.buildBreadcrumbs(this.route.root);
+  }
+
+  get currentPageTitle(): string {
+    return this.breadcrumbs.length > 0
+      ? this.breadcrumbs[this.breadcrumbs.length - 1].label
+      : '';
+  }
+
+  private buildBreadcrumbs(route: ActivatedRoute, url: string = '', breadcrumbs: Breadcrumb[] = []): Breadcrumb[] {
+    const children: ActivatedRoute[] = route.children;
+
+    if (children.length === 0) return breadcrumbs;
+
+    for (const child of children) {
+      const routeURL: string = child.snapshot.url.map(segment => segment.path).join('/');
+      if (routeURL !== '') {
+        url += `/${routeURL}`;
+        const label = child.snapshot.data['breadcrumb'] || routeURL;
+        breadcrumbs.push({ label, url });
+      }
+
+      return this.buildBreadcrumbs(child, url, breadcrumbs);
+    }
+
+    console.log(breadcrumbs)
+
+    return breadcrumbs;
+  }
+}
