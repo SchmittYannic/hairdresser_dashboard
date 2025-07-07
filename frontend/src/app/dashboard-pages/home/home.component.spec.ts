0157cc09bd59b079063a331fe337bbfa903e729c
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-07 13:42:59 +0200

    Update home.component.spec.ts

--- frontend/src/app/dashboard-pages/home/home.component.spec.ts
+++ frontend/src/app/dashboard-pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      imports: [HomeComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
