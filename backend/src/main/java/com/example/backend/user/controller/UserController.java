ca5d5af93d7957149f8e1e2a4c9cfce111355695
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-09 16:25:20 +0200

    Create UserController.java

---
+++ backend/src/main/java/com/example/backend/user/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.backend.user.controller;
+
+import com.example.backend.auth.service.AuthService;
+import com.example.backend.common.domain.model.User;
+import com.example.backend.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity<Map<String, Object>> getUsers(
+            @RequestParam(defaultValue = "0") int offset,
+            @RequestParam(defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "lastname") String sortField,
+            @RequestParam(defaultValue = "asc") String sortOrder,
+            @RequestParam(required = false) String lastname,
+            @RequestParam(required = false) String firstname,
+            @RequestParam(required = false) List<String> roles
+    ) {
+        Page<User> page = userService.getUsers(offset, limit, sortField, sortOrder, lastname, firstname, roles);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("users", page.getContent());
+        response.put("total", page.getTotalElements());
+
+        return ResponseEntity.ok(response);
+    }
+}
