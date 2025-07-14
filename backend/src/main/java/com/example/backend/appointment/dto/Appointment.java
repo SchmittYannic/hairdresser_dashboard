ab11f2c94909ed2cc2834721f2aa2aff6e1d1851
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-14 14:50:20 +0200

    Create Appointment.java

---
+++ backend/src/main/java/com/example/backend/appointment/dto/Appointment.java
@@ -0,0 +1,37 @@
+package com.example.backend.appointment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Document(collection = "appointments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Appointment {
+
+    @Id
+    private String id;
+
+    private String employee; // employee ID
+    private String customer; // customer ID
+
+    @Field("service_name")
+    private String serviceName;
+
+    private Integer duration;
+    private Date start;
+    private Date end;
+    private String remarks;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+}
