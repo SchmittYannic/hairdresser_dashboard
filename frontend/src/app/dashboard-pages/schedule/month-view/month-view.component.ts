865dbf914d4e55cc72efb87b5e945e0c654b7bfa
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-15 17:09:38 +0200

    Adds month-view component

---
+++ frontend/src/app/dashboard-pages/schedule/month-view/month-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-month-view',
+  standalone: false,
+  templateUrl: './month-view.component.html',
+  styleUrl: './month-view.component.scss'
+})
+export class MonthViewComponent {
+
+}
