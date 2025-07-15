d1d285ce3c65a88f2c4bdd99b499e30408af3850
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-15 20:10:02 +0200

    Update schedule.module.ts

--- frontend/src/app/dashboard-pages/schedule/schedule.module.ts
+++ frontend/src/app/dashboard-pages/schedule/schedule.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
 
 import { ScheduleComponent } from './schedule.component';
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { CardComponent } from '@app/shared/components/card/card.component';
 import { DayViewComponent } from './day-view/day-view.component';
+import { ScheduleStore } from './schedule.store';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownComponent } from 'app/shared/components/dropdown/dropdown';
+import { WeekViewComponent } from './week-view/week-view.component';
+import { MonthViewComponent } from './month-view/month-view.component';
 
 @NgModule({
   declarations: [
     ScheduleComponent,
     DayViewComponent,
+    WeekViewComponent,
+    MonthViewComponent,
   ],
   imports: [
     CommonModule,
     ScheduleRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
     CardComponent,
+    DropdownComponent,
+  ],
+  providers: [
+    ScheduleStore,
   ],
   exports: [ScheduleComponent]
 })
