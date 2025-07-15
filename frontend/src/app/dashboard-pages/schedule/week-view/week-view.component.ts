2dad2bf6d25001a3db6ec3cfe2c30a4e1faa6719
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-15 17:09:25 +0200

    Adds week-view component

---
+++ frontend/src/app/dashboard-pages/schedule/week-view/week-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-week-view',
+  standalone: false,
+  templateUrl: './week-view.component.html',
+  styleUrl: './week-view.component.scss'
+})
+export class WeekViewComponent {
+
+}
