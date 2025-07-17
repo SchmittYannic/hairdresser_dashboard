bebd0f5ba5b40fc09b041c6e611d05e9b329b8d5
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-17 14:41:43 +0200

    Update appointment.model.ts

--- frontend/src/app/shared/models/appointment.model.ts
+++ frontend/src/app/shared/models/appointment.model.ts
@@ -1,10 +1,11 @@
 export interface Appointment {
   id: string;
+  employee: string;
+  customer: string;
+  customerFirstname: string;
+  customerLastname: string;
+  serviceName: string;
+  duration: number;
   start: string;
   end: string;
-  serviceName: string;
-  customer: string;
-  employee: string;
-  duration: number;
-  remarks?: string;
 }
