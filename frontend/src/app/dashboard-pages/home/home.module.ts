c9e010f58369b98786d529313365975d9b55155b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-07-20 19:33:17 +0200

    Use schedule-card in home component

--- frontend/src/app/dashboard-pages/home/home.module.ts
+++ frontend/src/app/dashboard-pages/home/home.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ScheduleCardComponent } from './widgets/schedule-card/schedule-card.component';
+import { CardComponent } from '@app/shared/components/card/card.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    ScheduleCardComponent,
   ],
   imports: [
     HomeRoutingModule,
     CommonModule,
+    CardComponent,
   ],
   providers: [
   ],
